03d636d8808045f0ad531b68d7d2a50fe1d2cd70
Frank Borja <fborjaz@unemi.edu.ec>
2024-05-14 15:31:20 -0500

    App config

--- app.js
+++ app.js
@@ -13,16 +13,10 @@ app.set('view engine', 'ejs');
 // Middleware para servir archivos estáticos
 app.use(express.static(join(__dirname, 'public')));
 
-// Middleware para las rutas de la API
-app.use('/api', indexRoutes);
+// Middleware para las rutas
+app.use('/', indexRoutes);
 
-// Middleware para manejar errores específicos de las rutas
-app.use('/api', (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Hubo un error en la ruta de la API' });
-});
-
-// Manejo de errores generales
+// Manejo de errores
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Hubo un error en el servidor');
